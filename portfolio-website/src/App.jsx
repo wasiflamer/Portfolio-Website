38a43a9cfa118f73ba1cf059a1989e79c63384ea
wasiflamer <wasiflamer@gmail.com>
2023-08-01 20:36:48 +0500

    autosafe settings aghhhhhhh

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,10 +1,12 @@
-
-
 function App() {
   return (
     <>
-      <button class="btn btn-secondary w-64 mt-10 ml-10">Button</button>
+      <div className='navbar-center p-1 bg-secondary text-primary-content mt-10 ml-10 mr-10 rounded-2xl'>
+        <div className='flex-1'>
+          <a className='btn btn-ghost normal-case text-xl'>Waseem</a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
