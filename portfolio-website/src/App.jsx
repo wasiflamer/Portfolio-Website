75c421ba4d47cb8f34152608bc065c91814319dc
wasiflamer <wasiflamer@gmail.com>
2023-08-01 14:17:05 +0500

    theme added

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,8 +1,9 @@
 
+
 function App() {
   return (
     <>
-    <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary w-64 mt-10 ml-10">Button</button>
     </>
   )
 }
