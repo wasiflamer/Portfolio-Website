9670f582d9311a1b7fd8443451f581e4ab1bb7a0
wasiflamer <wasiflamer@gmail.com>
2023-08-02 19:09:47 +0500

    mock code + hero image

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -2,19 +2,27 @@ function Hero() {
   return (
     <div className='hero min-h-full bg-base-900 '>
       <div className='hero-content flex-col lg:flex-row-reverse'>
-        <img
-          src='https://images.pexels.com/photos/17029537/pexels-photo-17029537/free-photo-of-young-woman-in-a-modern-denim-outfit.jpeg'
-          className='max-w-sm rounded-lg shadow-2xl'
-        />
+        <div className='stack w-full h-full'>
+          <img
+            src='https://images.pexels.com/photos/17029537/pexels-photo-17029537/free-photo-of-young-woman-in-a-modern-denim-outfit.jpeg'
+            className='max-w-sm  shadow-2xl mask mask-parallelogram-3'
+          />
+          <img
+            src='https://images.pexels.com/photos/17029537/pexels-photo-17029537/free-photo-of-young-woman-in-a-modern-denim-outfit.jpeg'
+            className='max-w-md  shadow-2xl mask mask-parallelogram-4'
+          />
+        </div>
         <div>
-          <h1 className='text-5xl font-bold'>Stuff Stuff</h1>
+          <h1 className='text-5xl font-bold'>I AM WASEEM</h1>
           <p className='py-6 text-primary-content mr-10'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
-            obcaecati, dolores id ea perferendis ducimus. Obcaecati facilis,
-            commodi, cupiditate voluptates molestias doloremque ipsa, iste sunt
-            praesentium atque provident veritatis facere!
+            A passionate React developer with a keen eye for detail and a love
+            for innovation. I thrive on crafting captivating web experiences
+            with clean code, dedicated to delivering so lutions that leave a
+            lasting impact. Let's build something incredible together!"
           </p>
-          <button className='btn btn-primary w-60 normal-case'>Contact</button>
+          <button className=' btn btn-primary btn-wide text-primary-content normal-case'>
+            Contact
+          </button>
         </div>
       </div>
     </div>
@@ -25,7 +33,9 @@ function Bar() {
   return (
     <div className='navbar-center bg-secondary text-primary-content m-20 p-2 rounded-full shadow-md'>
       <div className='flex-1'>
-        <a className='btn btn-accent normal-case text-xl '>Waseem Raza</a>
+        <a className='btn btn-ghost normal-case text-xl bg-slate-50 text-primary-content hover:bg-slate-50'>
+          Portfolio
+        </a>
       </div>
     </div>
   );
@@ -38,6 +48,16 @@ function App() {
       <div className='divider divider-vertical mx-24'></div>
       <Hero />
       <div className='divider divider-vertical mx-24'></div>
+
+      <div className='flex justify-center'>
+        <div className='mockup-code m-10 bg-gradient-to-r from-[#8360c3] to-[#2ebf91] '>
+          <pre data-prefix='$'>
+            <code className='bg-slate-50 text-primary-content'>
+              Some of my hobby coded projects !
+            </code>
+          </pre>
+        </div>
+      </div>
     </>
   );
 }
