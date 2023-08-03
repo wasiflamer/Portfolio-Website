edf9bc369ba248bd4326b636e2f85b8dd116513a
wasiflamer <wasiflamer@gmail.com>
2023-08-03 18:55:52 +0500

    some quality changes

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -3,21 +3,22 @@ function Hero() {
     <div className='hero min-h-full bg-base-900 '>
       <div className='hero-content flex-col lg:flex-row-reverse'>
         <div className='stack w-full h-full'>
-          <img src='/images/wasi.jpg' className='max-w-xl mask mask-squircle' />
           <img
             src='/images/wasi.jpg'
-            className='max-w-full max-h-full mask mask-squircle '
+            className='max-w-xl mask mask-squircle bg-gradient-to-r from-cyan-500 to-blue-500 '
           />
         </div>
         <div>
-          <h1 className='text-5xl font-bold'>I AM WASEEM</h1>
+          <h1 className='text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#8360c3] to-[#2ebf91]'>
+            I AM WASEEM
+          </h1>
           <p className='py-6 text-primary-content mr-10'>
             A passionate React developer with a keen eye for detail and a love
             for innovation. I thrive on crafting captivating web experiences
             with clean code, dedicated to delivering so lutions that leave a
             lasting impact. Let's build something incredible together!"
           </p>
-          <button className=' btn btn-primary btn-wide text-primary-content normal-case'>
+          <button className=' btn btn-primary btn-wide text-white normal-case '>
             Contact
           </button>
         </div>
