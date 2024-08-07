a775911a2d8b9b2d833f75c726aa0cc056b1dd10
Muhammad Waseem Raza <wasiflamer@gmail.com>
2024-08-08 00:58:54 +0500

    happy with color

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function Codeblock() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center  bg-[#2E294E]">
       <div className="mockup-code m-10 bg-gradient-to-r from-[#8360c3] to-[#2ebf91] ">
         <pre data-prefix="$">
           <code className=" text-white text-bold text-lg ">
@@ -17,7 +17,7 @@ function Codeblock() {
 
 function Showcase() {
   return (
-    <div className="flex flex-col space-y-4 justify-center text-center items-center">
+    <div className="flex flex-col space-y-4 justify-center text-center items-center bg-[#2E294E]">
       <div className="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral rounded-box">
         <div id="item1" className="carousel-item">
           <img
@@ -50,73 +50,79 @@ function Showcase() {
 
 function Hero() {
   return (
-    <div className="hero min-h-full bg-base-900 ">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="drop-shadow-2xl">
-          <img src="/images/wasi.jpg" className="max-w-lg mask mask-circle " />
-        </div>
-        <div className="bg-gradient-to-r from-[#10d87b] to-[#06853b] p-10 rounded-lg">
-          <h1
-            className="text-8xl font-bold bg-clip-text
-           text-transparent bg-gradient-to-r from-[#00aeff] to-[#000000]"
-          >
-            Hey !
-          </h1>
-
-          <h1
-            className="text-5xl font-bold bg-clip-text
-           text-transparent bg-gradient-to-r from-[#1D976C] to-[#93F9B9]"
-          >
-            I AM WASEEM
-          </h1>
-          <p className="py-6 text-primary-content mr-10">
-            A passionate React developer with a keen eye for detail and a love
-            for innovation. I thrive on crafting captivating web experiences
-            with clean code, dedicated to delivering solutions that leave a
-            lastingimpact Let's build something incredible together!"
-          </p>
-          <button
-            className="btn btn-neutral
-           btn-wide text-white normal-case  hover:text-neutral hover:bg-white "
-          >
-            Hire Me
-          </button>
-          <div className="mt-6 font-bold text-4xl">
-            <h1>Things i know well </h1>
+    <>
+      <Bar />
+      <div className="hero min-h-full bg-gradient-to-b from-[#011638] to-[#011028] ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="drop-shadow-2xl">
+            <img
+              src="/images/wasi.png"
+              className="max-w-lg mask mask-circle "
+            />
           </div>
-          <div className="flex mt-10 gap-4 flex-row">
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
-                alt=""
-                className="w-18 h-12"
-              />
+          <div className="bg-gradient-to-r from-[#E8C547] to-[#E8C547] p-10 rounded-lg">
+            <h1
+              className="text-8xl font-bold bg-clip-text
+           text-transparent bg-gradient-to-r from-[#011638] to-[#000000]"
+            >
+              Hey !
+            </h1>
+
+            <h1
+              className="text-5xl font-bold bg-clip-text
+           text-transparent bg-gradient-to-r from-[#1D976C] to-[#93F9B9]"
+            >
+              I AM WASEEM
+            </h1>
+            <p className="py-6 text-primary-content mr-10">
+              A passionate React developer with a keen eye for detail and a love
+              for innovation. I thrive on crafting captivating web experiences
+              with clean code, dedicated to delivering solutions that leave a
+              lastingimpact Let's build something incredible together!"
+            </p>
+            <button
+              className="btn btn-neutral
+           btn-wide text-white normal-case  hover:text-neutral hover:bg-white "
+            >
+              Hire Me
+            </button>
+            <div className="mt-6 font-bold text-4xl">
+              <h1>Things i know well </h1>
             </div>
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
-                alt=""
-                className="w-18 h-12"
-              />
-            </div>
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
-                alt=""
-                className="w-18 h-12"
-              />
+            <div className="flex mt-10 gap-4 flex-row">
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
+                  alt=""
+                  className="w-18 h-12"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
+                  alt=""
+                  className="w-18 h-12"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
+                  alt=""
+                  className="w-18 h-12"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 function Bar() {
   return (
     <div className="m-6">
-      <div className="navbar bg-primary text-primary-content mx-2 my-2 rounded-full shadow-md">
+      <div className="navbar bg-transparent text-primary-content mx-2 my-2 rounded-md shadow-md">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -169,10 +175,7 @@ function Bar() {
 function App() {
   return (
     <>
-      <Bar />
-      <div className="divider divider-vertical mx-24"></div>
       <Hero />
-      <div className="divider divider-vertical mx-24"></div>
       <Codeblock />
       <Showcase />
     </>
