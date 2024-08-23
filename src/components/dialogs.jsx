69965c9c4e604b27e61e1b29476cff60800aebee
Muhammad Waseem Raza <wasiflamer@gmail.com>
2024-08-23 10:11:28 +0500

    description added with bug

--- src/components/dialogs.jsx
+++ src/components/dialogs.jsx
@@ -1,6 +1,6 @@
 // import font awesome arrow right icon here
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import Carousel from "./carousel";
 
@@ -20,12 +20,8 @@ export default function Dialogs() {
                 ✕
               </button>
             </form>
-            <div>
-              <h3 className="font-bold text-lg">GameRaid</h3>
-              <p className="py-4">Video Game Tracker And Collection Manager</p>
-            </div>
           </div>
-          <div className="flex flex-row justify-start gap-4 items-stretch border-2 border-red-500">
+          <div className="flex flex-row justify-start gap-2 border-2 border-red-500">
             <div className="mockup-phone border-primary m-0">
               <div className="camera"></div>
               <div className="display">
@@ -35,9 +31,43 @@ export default function Dialogs() {
               </div>
             </div>
             <div className="self-center">
-              <button className="">
-                <FontAwesomeIcon icon={faArrowAltCircleRight} size="3x" />
-              </button>
+              <a className="">
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  size="3x"
+                  className="hover:animate-tada"
+                />
+              </a>
+            </div>
+            <div className="flex m-10 items-start border-2 border-yellow-600">
+              <div className="">
+                <h3 className="font-bold text-lg">GameRaid</h3>
+                <p className="py-4">
+                  Video Game Tracker And Collection Manager
+                </p>
+                <h3 className="font-bold text-lg">Features</h3>
+                <ul className="list-disc ml-4 py-2">
+                  <li>Wishlist</li>
+                  <li>New Releases</li>
+                  <li>Catagories</li>
+                  <li>Search</li>
+                </ul>
+                <h3 className="font-bold text-lg py-2">Description</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+                  voluptatibus repudiandae omnis eum commodi provident animi
+                  dicta. Dolor suscipit vel ipsa id aut quam possimus
+                  asperiores, impedit odit officia nisi? Lorem ipsum dolor sit
+                  amet consectetur adipisicing elit. Perspiciatis, aut
+                  recusandae! Laboriosam distinctio, ipsam quaerat, facere,
+                  error consequatur ullam alias quia quos suscipit libero
+                  nesciunt quae atque? Repudiandae, ipsam culpa. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Fugiat repellat
+                  temporibus laudantium consequatur perspiciatis itaque, ex
+                  eligendi voluptatum neque consequuntur, rerum alias. Sequi
+                  ipsam repellat, ullam alias similique fugit amet?
+                </p>
+              </div>
             </div>
           </div>
 
