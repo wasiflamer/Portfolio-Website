93d4f1c641ed23d5f6f081ea6d47c512bf7265cf
Muhammad Waseem Raza <wasiflamer@gmail.com>
2024-08-23 08:31:20 +0500

    modal under contruction

---
+++ src/components/carousel.jsx
@@ -0,0 +1,57 @@
+export default function Carousel() {
+  return (
+    <>
+      <div className="carousel rounded-box w-64">
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+            className="w-full"
+            alt="Tailwind CSS Carousel component"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/dialogs.jsx
+++ src/components/dialogs.jsx
@@ -1,20 +1,54 @@
+// import font awesome arrow right icon here
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+
+import Carousel from "./carousel";
+
 export default function Dialogs() {
   return (
     <>
-      <dialog id="modal_1" className="modal text-gray-900">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4 rounded-lg bg-red-400">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">GameRaid</p>
+      <dialog
+        id="modal_1"
+        className="modal bg-[url('/images/background.jpg')] bg-center text-gray-900 "
+      >
+        <div className="modal-box min-h-[90%] min-w-[95%]">
+          {/* model screen  */}
+          <div className="flex flex-col border-2 border-green-500">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost rounded-lg absolute right-4 top-4 bg-red-400">
+                ✕
+              </button>
+            </form>
+            <div>
+              <h3 className="font-bold text-lg">GameRaid</h3>
+              <p className="py-4">Video Game Tracker And Collection Manager</p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-start gap-4 items-stretch border-2 border-red-500">
+            <div className="mockup-phone border-primary m-0">
+              <div className="camera"></div>
+              <div className="display">
+                <div className="artboard artboard-demo phone-1 ">
+                  <Carousel />
+                </div>
+              </div>
+            </div>
+            <div className="self-center">
+              <button className="">
+                <FontAwesomeIcon icon={faArrowAltCircleRight} size="3x" />
+              </button>
+            </div>
+          </div>
+
+          {/* ------------------ */}
         </div>
       </dialog>
-      <dialog id="modal_2" className="modal text-gray-900">
-        <div className="modal-box">
+      <dialog
+        id="modal_2"
+        className="modal bg-[url('/images/background.jpg')] bg-center text-gray-900 "
+      >
+        <div className="modal-box min-h-[90%] min-w-[95%]">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
@@ -25,8 +59,11 @@ export default function Dialogs() {
           <p className="py-4">Notecome</p>
         </div>
       </dialog>
-      <dialog id="modal_3" className="modal text-gray-900">
-        <div className="modal-box">
+      <dialog
+        id="modal_3"
+        className="modal bg-[url('/images/background.jpg')] bg-center text-gray-900"
+      >
+        <div className="modal-box min-h-[90%] min-w-[95%]">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
