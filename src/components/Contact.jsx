9c58ae343bac7090de5d106dc8304a9a4e174280
Muhammad Waseem Raza <wasiflamer@gmail.com>
2024-09-23 04:33:36 +0500

    some more progress with email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,6 @@ export default function ContactUs() {
   // handler
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs
       .sendForm("service_pol2wuo", "template_g4mybhi", form.current, {
         publicKey: "WFg4ohYfy98be-33W",
@@ -23,14 +22,69 @@ export default function ContactUs() {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>reply_to</label>
-      <input type="text" name="reply_to" id="reply_to" />
-      <label>from_name</label>
-      <input type="email" name="from_name" id="from_name" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input className="btn btn-square" type="submit" value="Send" />
-    </form>
+    <div className="hero bg-indigo-950 min-h-screen ">
+      <div className="hero-content flex-col lg:flex-col">
+        <div className="text-center text-white">
+          <h1 className="text-5xl font-bold ">I'm Here</h1>
+          <p className="py-6">Discuss your project or problem !</p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body" ref={form} onSubmit={sendEmail}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="input input-bordered"
+                required
+                name="reply_to"
+                id="reply_to"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered"
+                required
+                name="from_name"
+                id="from_name"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Message</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your Queries and Concerns ! "
+                className="input input-bordered"
+                required
+                id="message"
+                name="message"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-secondary" type="submit" value="Send" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
+
+//  <form ref={form} onSubmit={sendEmail}>
+//       <label>reply_to</label>
+//       <input type="text" name="reply_to" id="reply_to" />
+//       <label>from_name</label>
+//       <input type="email" name="from_name" id="from_name" />
+//       <label>Message</label>
+//       <textarea name="message" />
+//       <input className="btn btn-square" type="submit" value="Send" />
+//     </form>
--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,54 +1,7 @@
 export default function Email() {
   return (
     <>
-      <div className="hero bg-indigo-950 min-h-screen ">
-        <div className="hero-content flex-col lg:flex-col">
-          <div className="text-center text-white">
-            <h1 className="text-5xl font-bold ">I'm Here</h1>
-            <p className="py-6">Discuss your project or problem !</p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-secondary">Send</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      
     </>
   );
 }
