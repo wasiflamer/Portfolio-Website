0936e92cf8860d25a586da5a0c21555ab74860d6
wasiflamer <wasiflamer@gmail.com>
2025-07-26 23:06:11 +0500

    Update color of PolygonCard in Hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -23,7 +23,7 @@ export default function Hero() {
   <PolygonCard
       height="600px"
     width="600px"
-    color="#f43f5e"
+    color="#FFFFF"
     zIndex={2}
     className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-0"
   />
